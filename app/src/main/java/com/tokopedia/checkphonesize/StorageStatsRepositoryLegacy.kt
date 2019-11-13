70e840f6049b04c687924a928b000d950e25aef6
Adrian Hartanto <awi_dan@yahoo.com>
2019-11-13 11:01:39 +0700

    Update StorageStatsRepositoryLegacy working below API 18

--- app/src/main/java/com/tokopedia/checkphonesize/StorageStatsRepositoryLegacy.kt
+++ app/src/main/java/com/tokopedia/checkphonesize/StorageStatsRepositoryLegacy.kt
@@ -3,13 +3,15 @@ package com.tokopedia.checkphonesize
 import android.os.Build
 import android.os.Environment
 import android.os.StatFs
-import androidx.annotation.RequiresApi
 
 class StorageStatsRepositoryLegacy : StorageStatsRepository {
 
-    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
     override suspend fun getFreeSpaceBytes(): Long {
         val statFs = StatFs(Environment.getDataDirectory().absolutePath)
-        return statFs.availableBytes
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            statFs.availableBytes
+        } else {
+            (statFs.blockSize * statFs.availableBlocks).toLong()
+        }
     }
 }
